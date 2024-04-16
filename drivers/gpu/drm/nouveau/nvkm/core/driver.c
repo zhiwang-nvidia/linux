5fa0511b52836b0ac835ed61700a493cf639b829
Ben Skeggs <bskeggs@nvidia.com>
2024-04-16 16:15:50 +1000

    drm/nouveau/nvkm: move nouveau_nvif.c to nvkm/core/driver.c
    - move to a more appropriate place before making changes
    
    Signed-off-by: Ben Skeggs <bskeggs@nvidia.com>

--- drivers/gpu/drm/nouveau/nvkm/core/driver.c
+++ drivers/gpu/drm/nouveau/nvkm/core/driver.c
@@ -27,49 +27,48 @@
  ******************************************************************************/
 
 #include <core/client.h>
+#include <core/event.h>
 #include <core/ioctl.h>
 
-#include <nvif/client.h>
 #include <nvif/driver.h>
 #include <nvif/event.h>
-#include <nvif/ioctl.h>
-
-#include "nouveau_drv.h"
 
 static void
-nvkm_client_unmap(void *priv, void __iomem *ptr, u32 size)
+nvkm_driver_unmap(void *priv, void __iomem *ptr, u32 size)
 {
 	iounmap(ptr);
 }
 
 static void __iomem *
-nvkm_client_map(void *priv, u64 handle, u32 size)
+nvkm_driver_map(void *priv, u64 handle, u32 size)
 {
 	return ioremap(handle, size);
 }
 
 static int
-nvkm_client_ioctl(void *priv, void *data, u32 size, void **hack)
+nvkm_driver_ioctl(void *priv, void *data, u32 size, void **hack)
 {
 	return nvkm_ioctl(priv, data, size, hack);
 }
 
 static int
-nvkm_client_resume(void *priv)
+nvkm_driver_resume(void *priv)
 {
 	struct nvkm_client *client = priv;
+
 	return nvkm_object_init(&client->object);
 }
 
 static int
-nvkm_client_suspend(void *priv)
+nvkm_driver_suspend(void *priv)
 {
 	struct nvkm_client *client = priv;
+
 	return nvkm_object_fini(&client->object, true);
 }
 
 static int
-nvkm_client_event(u64 token, void *repv, u32 repc)
+nvkm_driver_event(u64 token, void *repv, u32 repc)
 {
 	struct nvif_object *object = (void *)(unsigned long)token;
 	struct nvif_event *event = container_of(object, typeof(*event), object);
@@ -81,20 +80,19 @@ nvkm_client_event(u64 token, void *repv, u32 repc)
 }
 
 static int
-nvkm_client_driver_init(const char *name, u64 device, const char *cfg,
-			const char *dbg, void **ppriv)
+nvkm_driver_init(const char *name, u64 device, const char *cfg, const char *dbg, void **ppriv)
 {
-	return nvkm_client_new(name, device, cfg, dbg, nvkm_client_event,
+	return nvkm_client_new(name, device, cfg, dbg, nvkm_driver_event,
 			       (struct nvkm_client **)ppriv);
 }
 
 const struct nvif_driver
 nvif_driver_nvkm = {
 	.name = "nvkm",
-	.init = nvkm_client_driver_init,
-	.suspend = nvkm_client_suspend,
-	.resume = nvkm_client_resume,
-	.ioctl = nvkm_client_ioctl,
-	.map = nvkm_client_map,
-	.unmap = nvkm_client_unmap,
+	.init = nvkm_driver_init,
+	.suspend = nvkm_driver_suspend,
+	.resume = nvkm_driver_resume,
+	.ioctl = nvkm_driver_ioctl,
+	.map = nvkm_driver_map,
+	.unmap = nvkm_driver_unmap,
 };
