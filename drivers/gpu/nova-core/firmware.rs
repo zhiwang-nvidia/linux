cc300652bbf14e84dd30c4d1e731e96b8c091686
Alexandre Courbot <acourbot@nvidia.com>
2025-05-02 14:44:12 +0900

    nova-core: firmware: use 570.144

--- drivers/gpu/nova-core/firmware.rs
+++ drivers/gpu/nova-core/firmware.rs
@@ -26,7 +26,7 @@ pub(crate) mod radix3;
 pub(crate) mod riscv;
 pub(crate) mod sec2;
 
-pub(crate) const FIRMWARE_VERSION: &str = "535.113.01";
+pub(crate) const FIRMWARE_VERSION: &str = "570.144";
 
 fn elf_section<'a, 'b>(elf: &'a [u8], name: &'b str) -> Option<&'a [u8]> {
     let hdr = elf
