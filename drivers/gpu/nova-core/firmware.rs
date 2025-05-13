39ffaed360af7dc360ea12e1e7ddf01b49a8cdb1
Joel Fernandes <joelagnelf@nvidia.com>
2025-05-13 16:47:29 -0400

    nova-core: Move impl_from_bytes to a common place
    This is needed by sequencer.
    
    Signed-off-by: Joel Fernandes <joelagnelf@nvidia.com>

--- drivers/gpu/nova-core/firmware.rs
+++ drivers/gpu/nova-core/firmware.rs
@@ -252,27 +252,6 @@ impl<F: FalconFirmware> FirmwareDmaObject<F, Unsigned> {
     }
 }
 
-// TODO: turn this into a local marker trait that is auto-implemented for structs implementing
-// `FromBytes`.
-macro_rules! impl_from_bytes {
-    ($name:ty) => {
-        impl $name {
-            pub(crate) fn from_bytes(bytes: &[u8]) -> Result<Self> {
-                let mut data: [u8; size_of::<Self>()] = bytes.try_into().map_err(|_| EINVAL)?;
-
-                const U32_SIZE: usize = size_of::<u32>();
-                data.chunks_exact_mut(U32_SIZE)
-                    .map(|slice| <&mut [u8; U32_SIZE]>::try_from(slice).unwrap())
-                    .for_each(|chunk| *chunk = u32::from_le_bytes(*chunk).to_ne_bytes());
-
-                // SAFETY: the `FromBytes` implementation guarantees that any byte stream is valid
-                // for `Self`.
-                Ok(unsafe { core::mem::transmute::<[u8; size_of::<Self>()], Self>(data) })
-            }
-        }
-    };
-}
-
 #[repr(C)]
 #[derive(Debug)]
 struct BinHdr {
