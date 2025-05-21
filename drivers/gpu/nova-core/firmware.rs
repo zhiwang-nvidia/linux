083b4e45211a5788ab392154b7ea1ad3a18c623e
Joel Fernandes <joelagnelf@nvidia.com>
2025-05-20 22:21:13 -0400

    nova-core: firmware: Get RiscvUcode descriptor from GSP-RM image
    This will be written into the FALCON_OS register during GSP boot.
    
    Signed-off-by: Joel Fernandes <joelagnelf@nvidia.com>

--- drivers/gpu/nova-core/firmware.rs
+++ drivers/gpu/nova-core/firmware.rs
@@ -89,6 +89,7 @@ pub(crate) struct Firmware {
     pub bootloader: RiscvFirmware,
     pub gsp: RadixFirmware,
     pub gsp_sigs: DmaObject,
+    pub gsp_desc: RmRiscvUCodeDesc,
 }
 
 impl Firmware {
@@ -108,9 +109,33 @@ impl Firmware {
         };
 
         let gsp_fw = request("gsp")?;
-        let gsp = elf_section(gsp_fw.data(), ".fwimage")
-            .ok_or(EINVAL)
-            .and_then(|data| RadixFirmware::new(dev, ".fwimage", data))?;
+        let gsp_elf = elf::Parser::new(gsp_fw.data())?;
+
+        let (gsp, gsp_desc) = {
+            // Extract the .fwimage section for the GSP firmware
+            let data = gsp_elf
+                .sections_iter()?
+                .filter_map(Result::ok)
+                .find(|section| section.name == ".fwimage")
+                .map(|section| section.data)
+                .ok_or(EINVAL)?;
+
+            let gsp = RadixFirmware::new(dev, ".fwimage", data)?;
+
+            // Extract RISC-V ucode descriptor
+            let hdr = data
+                .get(0..size_of::<BinHdr>())
+                .ok_or(EINVAL)
+                .and_then(BinHdr::from_bytes)?;
+
+            let offset = hdr.header_offset as usize;
+            let desc = data
+                .get(offset..offset + size_of::<RmRiscvUCodeDesc>())
+                .ok_or(EINVAL)
+                .and_then(RmRiscvUCodeDesc::from_bytes)?;
+
+            (gsp, desc)
+        };
 
         // TODO: make this a GPU-specific const.
         let gsp_sigs_section = ".fwsignature_ga10x";
@@ -126,6 +151,7 @@ impl Firmware {
             bootloader: request("bootloader").and_then(|fw| RiscvFirmware::new(dev, &fw))?,
             gsp,
             gsp_sigs,
+            gsp_desc,
         })
     }
 }
@@ -301,7 +327,7 @@ impl_from_bytes!(HsLoadHeaderV2App);
 
 #[repr(C)]
 #[derive(Debug)]
-struct RmRiscvUCodeDesc {
+pub(crate) struct RmRiscvUCodeDesc {
     version: u32,
     bootloader_offset: u32,
     bootloader_size: u32,
