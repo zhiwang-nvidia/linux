5980987789055497d528fa7705721488ff50f0b1
John Hubbard <jhubbard@nvidia.com>
2025-07-19 17:53:39 -0700

    nova-core: rustfmt fixes for falcon/gsp.rs
    Applied rustfmt, and also a tiny reordering of the imports at the top
    of the file.
    
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>

--- drivers/gpu/nova-core/falcon/gsp.rs
+++ drivers/gpu/nova-core/falcon/gsp.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 
-use kernel::time::Delta;
 use kernel::prelude::*;
+use kernel::time::Delta;
 
 use crate::{
     driver::Bar0,
@@ -27,11 +27,7 @@ impl Falcon<Gsp> {
     }
 
     /// Function to check if GSP reload/resume has completed during the boot process.
-    pub(crate) fn check_reload_completed(
-        &self,
-        bar: &Bar0,
-        timeout: Delta,
-    ) -> Result<bool> {
+    pub(crate) fn check_reload_completed(&self, bar: &Bar0, timeout: Delta) -> Result<bool> {
         wait_on(timeout, || {
             let val = regs::NV_PGC6_BSI_SECURE_SCRATCH_14::read(bar);
             if val.boot_stage_3_handoff() {
